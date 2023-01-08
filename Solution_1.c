2a8b451c0ece65c4e35f7ebe5d06be28acb4a23c
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2023-01-08 18:47:17 +0530

    upd

--- Solution_1.c
+++ Solution_1.c
@@ -6,8 +6,8 @@ int main(){
     scanf("%d",&n);
 
     for(int i = 1; i<=n; i++){
-        for(int j = 1;j<=2*n-1;j++){
-            if(j >= n-(i-1) && j<= n+(n-1))
+        for(int j = 1 ;j<=2*n-1; j++){
+            if(j >= n-(i-1) && j<= n+(i-1))
                 printf("*");
             else
                 printf(" ");
