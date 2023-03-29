9c4ac0b9431220d3960dc6418551bdcc333feb13
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2023-03-29 18:26:42 +0530

    upd

--- Solutions in Java/Solution_1.java
+++ Solutions in Java/Solution_1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Solution_1{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter No. of rows:");
+        System.out.print("Enter No. of rows: ");
         int rows = sc.nextInt();
         sc.close();
 
