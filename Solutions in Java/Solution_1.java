bdcb3d84c36978cd3205764184eb8f21492fd354
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2023-03-29 18:18:07 +0530

    upd

--- Solutions in Java/Solution_1.java
+++ Solutions in Java/Solution_1.java
@@ -7,8 +7,8 @@ public class Solution_1{
         int rows = sc.nextInt();
         sc.close();
 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < rows*2 -1 ; j++){
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= rows*2 -1 ; j++){
                 if (j >= rows - (i-1) && j <= rows + (i-1))
                     System.out.print("*");
                 else
--- Solutions in Java/Solution_2.java
+++ Solutions in Java/Solution_2.java
@@ -1,5 +1,1 @@
-import java.util.Scanner;
 
-public class Solution_2 {
-    public static  void main(String)
-}
